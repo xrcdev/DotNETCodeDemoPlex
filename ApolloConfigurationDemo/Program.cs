b641eca65cdcaf9542ae1ef4afb11aebede7a650
francis.xu <xrcdev@users.noreply.github.com>
2021-11-26 16:21:54 +0800

    Add Apollo configuration

---
+++ ApolloConfigurationDemo/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Com.Ctrip.Framework.Apollo.ConfigAdapter;
+namespace ApolloConfigurationDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, builder) => builder.AddApollo(builder.Build().GetSection("apollo")))
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
